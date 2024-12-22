85db08e1c6f757d202bf456c57b83c6be6f739c5
Mauro-Pereira <mauropereira1096@gmail.com>
2024-12-22 10:18:40 -0300

    feat: implementing the ProductCanNotBeLessThanZeroException class with RuntimeException extends

--- src/main/java/com/example/Inventory/exception/ProductCanNotBeLessThanZeroException.java
+++ src/main/java/com/example/Inventory/exception/ProductCanNotBeLessThanZeroException.java
@@ -1,5 +1,9 @@
 package com.example.Inventory.exception;
 
-public class ProductCanNotBeLessThanZeroException {
+public class ProductCanNotBeLessThanZeroException extends RuntimeException{
+
+    public ProductCanNotBeLessThanZeroException(String message){
+        super(message);
+    }
 
 }
