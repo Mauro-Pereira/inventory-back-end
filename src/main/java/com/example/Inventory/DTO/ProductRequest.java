f302c0846393cfa12fc25631d0301d8d7fa59aa4
Mauro-Pereira <mauropereira1096@gmail.com>
2025-01-05 14:14:27 -0300

    feat: add ProductRequest record file

---
+++ src/main/java/com/example/Inventory/DTO/ProductRequest.java
@@ -0,0 +1,5 @@
+package com.example.Inventory.DTO;
+
+public record ProductRequest() {
+
+}
--- src/main/java/com/example/Inventory/controller/ProductController.java
+++ src/main/java/com/example/Inventory/controller/ProductController.java
@@ -1,5 +1,13 @@
 package com.example.Inventory.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Inventory", description = "JWT Spring Boot API")
+@RestController
+@RequestMapping("/product")
 public class ProductController {
 
 }
