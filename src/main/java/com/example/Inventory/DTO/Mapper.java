10d2eb1f669db1d99fc794c2345b0325c89dd576
Mauro-Pereira <mauropereira1096@gmail.com>
2025-01-05 14:33:10 -0300

    feat: add Mapper implementation class

--- src/main/java/com/example/Inventory/DTO/Mapper.java
+++ src/main/java/com/example/Inventory/DTO/Mapper.java
@@ -1,5 +1,21 @@
 package com.example.Inventory.DTO;
 
+import com.example.Inventory.model.Product;
+
 public class Mapper {
+
+    public static Product toProduct(ProductRequest productRequest){
+        
+        Product product = new Product();
+        product.setName(productRequest.name());
+        product.setPrice(productRequest.price());
+        product.setQuantity(productRequest.quantity());
+        product.setDescription(productRequest.description());
+        return product;
+    }
+
+    public static ProductResponse toProductResponse(Product product){
+       return new ProductResponse(product.getId(), product.getName(), product.getQuantity(), product.getPrice(), product.getDescription());
+    }
 
 }
